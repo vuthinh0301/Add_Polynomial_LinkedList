49db606d28ecc922077c4d75367993f763204115
vuthinh0301 <vuthinh0301@gmail.com>
2022-03-07 09:46:13 +0700

    first commit

---
+++ Add_Polynomial/src/Node.java
@@ -0,0 +1,11 @@
+public class Node {
+    public  int coeff;
+    public int pow;
+    public Node next;
+    public Node(int coeff, int pow){
+        this.coeff = coeff;
+        this.pow = pow;
+        this.next = null;
+    }
+
+}
---
+++ Add_Polynomial/src/Polynomial.java
@@ -0,0 +1,36 @@
+public class Polynomial {
+    public static Node addPolynomial(Node p1, Node p2){
+
+        Node a = p1;
+        Node b = p2;
+        Node newHead = new Node(0,0);
+        Node c = newHead;
+
+        while(a != null || b != null){
+            if(a == null){
+                c.next = b;
+                break;
+            }
+            else if (b == null){
+                c.next = a;
+                break;
+            }
+            else if(a.pow == b.pow){
+                c.next = new Node(a.coeff + b.coeff, a.pow);
+                a = a.next;
+                b = b.next;
+            }
+            else if(a.pow > b.pow){
+                c.next = new Node(a.coeff, a.pow);
+                a = a.next;
+            }
+            else if(a.pow < b.pow){
+                c.next = new Node(b.coeff, b.pow);
+                b = b.next;
+            }
+            c = c.next;
+        }
+
+        return newHead.next;
+    }
+}
